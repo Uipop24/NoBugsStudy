b789639a8763c3da04606147d599d406c19bb8ef
maxim_f_a <staysoulbyme@gmail.com>
2026-08-06 18:04:22 +0400

    HW9

--- src/main/java/HomeWork9/MathConstants.java
+++ src/main/java/HomeWork9/MathConstants.java
@@ -1,22 +1,19 @@
 package HomeWork9;
 
 public class MathConstants {
-    final double PI = 3.14159;
-    final double E = 2.71828;
+    static final double PI = 3.14159;
+    static final double E = 2.71828;
 
-    static double calculateCircleArea(double r){
-        MathConstants mathConstants = new MathConstants();
-        return mathConstants.PI * r * r;
+    public static double calculateCircleArea(double r){
+        return PI * r * r;
     }
 
-    static double calculateCircumference(double r){
-        MathConstants mathConstants = new MathConstants();
-        return 2 * mathConstants.PI * r;
+    public static double calculateCircumference(double r){
+        return 2 * PI * r;
     }
 
-    public double calculateExponentialGrowth(double initialValue, double rate, double time){
-        MathConstants mathConstants = new MathConstants();
-        return initialValue * Math.pow(mathConstants.E, (rate * time));
+    public static double calculateExponentialGrowth(double initialValue, double rate, double time){
+        return initialValue * Math.pow(E, rate * time);
     }
 
     public static void main(String[] args) {
